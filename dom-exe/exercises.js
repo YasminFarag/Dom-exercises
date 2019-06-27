72cfee610e64c02a5b88f2852852b1a95ef38ad3
yasmin <yasminefawzy22@gmail.com>
2019-06-27 15:51:02 +0200

    new-exercise

--- dom-exe/exercises.js
+++ dom-exe/exercises.js
@@ -49,7 +49,13 @@ function multiply(){
     // third exercise
 
 
+    function sphereVolume(){
+        let radius = document.getElementById('radius').value
+        // radius = Math.abs(radius);
+        let volume = (4/3)* Math.PI * Math.pow(radius,3);
+        document.getElementById('volume').value = volume.toFixed(2)
+    }
     
-    
+    document.getElementById('calculate').onsubmit = sphereVolume
 
     
