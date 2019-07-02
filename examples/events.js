2fa550ad8ef18f5980f70bb708a483517ba3ca78
yasmin <yasminefawzy22@gmail.com>
2019-07-02 23:33:49 +0200

    modified

--- examples/events.js
+++ examples/events.js
@@ -1,5 +1,25 @@
-function changeText(){
+
+// Function to modify the text content of the paragraph
+const changeText=()=>{
     const p =document.querySelector('p')
     p.textContent= 'this paragraph is changed'
 }
 
+
+/* const p =document.querySelector('p')
+
+ const changeText= ()=>{
+     p.textContent= 'will i change?!'
+ } */
+
+ const button = document.querySelector('button');
+
+ const alerted =()=>{
+     alert('will i alert?!')
+ }
+
+//  events can be overwritten
+
+
+button.onclick = changeText;
+button.onclick = alerted;
