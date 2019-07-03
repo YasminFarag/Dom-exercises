ee29a8b652ef4dc9bc8a55ce84bf90dcb7c85855
yasmin <yasminefawzy22@gmail.com>
2019-07-03 10:25:42 +0200

    updates

--- examples/events.js
+++ examples/events.js
@@ -12,14 +12,41 @@ const changeText=()=>{
      p.textContent= 'will i change?!'
  } */
 
- const button = document.querySelector('button');
+ 
 
- const alerted =()=>{
-     alert('will i alert?!')
- }
+//  const alerted =()=>{
+//      alert('will i alert?!')
+//  }
 
 //  events can be overwritten
 
 
-button.onclick = changeText;
-button.onclick = alerted;
+//button.onclick = changeText;
+// button.onclick = alerted;
+
+
+// In the above example, the button click would only display an alert, and not change the p text, since the alert() code was the last one added to the property
+
+
+
+// Event Listeners
+
+
+// listen for event 'click'
+const button = document.querySelector('button');
+button.addEventListener('click', changeText)
+
+// An anonymous function on an event listener
+ // An anonymous function is a function that is not named
+
+
+
+
+ // Test the keyCode, key, and code properties
+
+ document.addEventListener('keydown', event=>{
+     console.log(`keycode:${event.keyCode}`);
+     console.log(`key:${event.key}`);
+     console.log(`code:${event.code}`);
+     
+ })
