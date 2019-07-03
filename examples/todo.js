0e1e97add98a7ca5f68cbe8de5da7cd93ce1e57d
yasmin <yasminefawzy22@gmail.com>
2019-07-04 00:13:08 +0200

    changes

--- examples/todo.js
+++ examples/todo.js
@@ -1,15 +1,31 @@
+const ul = document.querySelector('ul')
+
 const button = document.querySelector('button');
 
 button.addEventListener('click', ()=>{
-    console.log('yot clicked me!');
-    
+    //console.log('yot clicked me!');
+
+    // first way to creat an element
+
+   ul.innerHTML += '<li>new Task</li>'
+
+   // another way to add an element
+
+//    let li = document.createElement('li');
+//         li.textContent= 'New Task';
+//         ul.append(li)  // will be added to the end
+//        // ul.prepend(li)  // will be added to the top
+
 });
+
+
+
 const listedItems= document.querySelectorAll('li');
 
 listedItems.forEach(item =>{
     item.addEventListener('click',(event)=>{
         event.target.style.textDecoration= 'line-through';
-        
+        //event.target.remove(); // will remove each element will be selected
         
     })
 }) ;
