9f5ac832ab229d3f9a23840e91bc3b25ca9cc2aa
yasmin <yasminefawzy22@gmail.com>
2019-07-04 14:37:25 +0200

    first exe

--- exercises/jQuery/double-events.js
+++ exercises/jQuery/double-events.js
@@ -4,14 +4,31 @@
 // * When the user's mouse leaves, change the text again to 'Dont go please!'.
 
 $('#btn').html('Welcome! Stay forever')
+$('#btn').css('background-color','red')
+$('#btn').css('height','50px')
+
+
+$('#btn').click(function(){
+   $('#btn').css({
+       'color' :'white',
+       'margin-right': '50px'
+        
+   })
+})
 
 $(document).ready(function(){
-    $('#btn').on('mouseenter',function(e){
+    $('#btn').on('mouseenter mouseleave',function(e){
         if(e.type === 'mouseenter'){
             $('#btn').html('Dont leave me this way!')
+        } else if(e.type === 'mouseleave'){
+            $('#btn').html('just put the mouse over me!')
         }else{
-            $('#btn').css('color', 'green')
+            $('#btn').css('color','black')
         }
+        console.log(e.type);
+        
+            
+        
 
     })
 })
