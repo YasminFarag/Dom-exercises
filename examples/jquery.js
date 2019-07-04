b1599017d617811bc43b6951329234e8abb1a069
yasmin <yasminefawzy22@gmail.com>
2019-07-04 11:07:25 +0200

    new Example

--- examples/jquery.js
+++ examples/jquery.js
@@ -1,6 +1,34 @@
 //Selecting Elements by ID
 
-// $('#demo');
+$('#demo').html('hello World!');
+
+
+
+// jQuery SELECTORS
+
+$(document).ready(function(){
+     $('#demo').html('hello leute!')
+})
+
+
+// EVENTS
+
+$(document).ready(function(){
+    $('#trigger').click(function(){
+        $('#demo').html('Hallo World!!')
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
 
 $(document).ready(function(){
     $('p').click(function(){
