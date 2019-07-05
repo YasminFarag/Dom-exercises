d4af552e6ed4c8356ce56390fde5d08f90b5e939
yasmin <yasminefawzy22@gmail.com>
2019-07-05 19:47:05 +0200

    new styles

--- exercises/jQuery/drop-down.js
+++ exercises/jQuery/drop-down.js
@@ -1,3 +1,10 @@
 function press(){
     let drop= document.querySelector('.dropdown-content')
+    drop.addEventListener('mouseover', function(e){
+        if(e.type=== 'mouseover'){
+            drop.style.display= ('none')
+
+        }
+
+    })
 }
