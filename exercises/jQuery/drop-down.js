805a02c62c19058d4b7dd25a2f42d31162c4693b
yasmin <yasminefawzy22@gmail.com>
2019-07-06 01:11:29 +0200

    jQuery function

--- exercises/jQuery/drop-down.js
+++ exercises/jQuery/drop-down.js
@@ -1,8 +1,20 @@
 
+  // JS 
+  
   document.querySelector('.dropdown').addEventListener('mouseover',openMenu)
 
   function openMenu(){
       document.querySelector('.dropdown-content').classList.toggle('show')
 
 
+  }
+
+
+
+  // jQuery
+
+
+  $('.dropdown').addEventListener('mouseover',openMenu)
+  function openMenu(){
+    $('.dropdown-content').toggleClass('show')
   }
