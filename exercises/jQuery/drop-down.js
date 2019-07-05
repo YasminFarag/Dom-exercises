828ca168cbacbc834003c3152558c4d3fc2bbebf
yasmin <yasminefawzy22@gmail.com>
2019-07-06 00:34:10 +0200

    js done

--- exercises/jQuery/drop-down.js
+++ exercises/jQuery/drop-down.js
@@ -1,23 +1,3 @@
-/* function press(){
-    let drop= document.querySelector('#mainul')
-//     drop.addEventListener('mouseover', function(e){
-//         if(e.type=== 'mouseover'){
-//             drop.style.display= ('none')
-
-//         }
-
-//     })
-// }
-
-    if(drop.className === 'mymenu'){
-        drop.className += 'responsive'
-    }else{
-        drop.className = 'mymenu'
-    }
-}
- */
-
-
 
   document.querySelector('.dropdown').addEventListener('mouseover',openMenu)
 
