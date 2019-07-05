1924dc4f57fa72c866ad720df7b134940727ba21
yasmin <yasminefawzy22@gmail.com>
2019-07-05 22:53:04 +0200

    dropdown

--- exercises/jQuery/drop-down.js
+++ exercises/jQuery/drop-down.js
@@ -1,10 +1,28 @@
 function press(){
-    let drop= document.querySelector('.dropdown-content')
-    drop.addEventListener('mouseover', function(e){
-        if(e.type=== 'mouseover'){
-            drop.style.display= ('none')
+    let drop= document.querySelector('#mainul')
+//     drop.addEventListener('mouseover', function(e){
+//         if(e.type=== 'mouseover'){
+//             drop.style.display= ('none')
 
-        }
+//         }
 
-    })
+//     })
+// }
+
+    if(drop.className === 'mymenu'){
+        drop.className += 'responsive'
+    }else{
+        drop.className = 'mymenu'
+    }
 }
+
+
+/* function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+ */
