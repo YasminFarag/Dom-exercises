fcb601ddb6e8daeea9466dbeb72539de23b42b93
yasmin <yasminefawzy22@gmail.com>
2019-07-08 10:31:13 +0200

    function

--- exercises/jQuery/todo-list.js
+++ exercises/jQuery/todo-list.js
@@ -1,9 +1,35 @@
 // JS
 
-document.querySelector('#btn').addEventListener('mouseenter',addItem)
+/* 
+
+button.addEventListener('keypress',(e)=>{
+    if(e.key === 'keypress'){
+        let lis = document.createElement('li');
+        lis.textContent= 'task'
+    }
+
+}); */
+
+let button = document.querySelector('#btn');
+myCont = document.querySelector('.container')
+button.addEventListener('click',()=>{
 
 
-function addItem(){
+    let listedItem= document.createElement('li')
+   
+    listedItem.textContent= document.querySelector('#textInput').value
 
-    
-}
+    myCont.appendChild(listedItem)
+
+    document.body.appendChild(myCont)
+
+
+
+})
+
+/* function press(){
+
+   
+} */
+
+
