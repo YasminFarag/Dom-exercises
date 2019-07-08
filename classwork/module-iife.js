a92b9163ed61bacc65f3f01cdc83070af223f4d1
yasmin <yasminefawzy22@gmail.com>
2019-07-08 11:46:00 +0200

    modules

---
+++ classwork/module-iife.js
@@ -0,0 +1,12 @@
+// immediately invokes function expression
+
+// to call the function by () only
+
+(function(){
+    let x = 20;
+    let y = 20
+
+    let result = x + y;
+    console.log(result);
+    
+})();
