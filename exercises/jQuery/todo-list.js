81038b0c38db3a7d7b8dd19df0757987f5d886e2
yasmin <yasminefawzy22@gmail.com>
2019-07-08 11:15:55 +0200

    delete button

--- exercises/jQuery/todo-list.js
+++ exercises/jQuery/todo-list.js
@@ -14,7 +14,11 @@ let button = document.querySelector('#btn');
 myCont = document.querySelector('.main')
 button.addEventListener('click',()=>{
 
-    /* let ul= document.createElement('ul') */
+    /* let ul= document.createElement('ul')
+    ul.textContent= 'new Tasks'
+    myCont.appendChild(ul)
+    document.body.appendChild(myCont) */
+
     let listedItem= document.createElement('li')
    
     listedItem.textContent= document.querySelector('#textInput').value
@@ -25,11 +29,37 @@ button.addEventListener('click',()=>{
 
 
 
-})
-
-/* function press(){
-
-   
-} */
+    // delete button
 
 
+        let delBtn = document.createElement('button');
+        delBtn.textContent ='Delete'
+        delBtn.className= 'delete'
+
+        listedItem.appendChild(delBtn);
+        document.body.appendChild(listedItem)
+
+
+    let clickOnBtn = document.getElementsByClassName('delete')
+
+});
+
+
+
+
+
+
+
+
+
+
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
