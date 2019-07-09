3be7525686b95d04bb77936d5b6cf5d834e57303
yasmin <yasminefawzy22@gmail.com>
2019-07-09 15:39:25 +0200

    vanilla js

--- exercises/jQuery/todo-list.js
+++ exercises/jQuery/todo-list.js
@@ -14,8 +14,8 @@ let button = document.querySelector('#btn');
 myCont = document.querySelector('.main')
 button.addEventListener('click',()=>{
 
-    /* let ul= document.createElement('ul')
-    ul.textContent= 'new Tasks'
+    let ul= document.createElement('ul')
+   /*  ul.textContent= 'new Tasks'
     myCont.appendChild(ul)
     document.body.appendChild(myCont) */
 let listed= document.getElementById('list')
@@ -25,57 +25,60 @@ let listed= document.getElementById('list')
 
     listed.appendChild(listedItem)
 
-    //document.body.appendChild(listed)
+    //styling listed item
 
-
+     listed.style.listStyleType= 'none';
+    listed.style.fontSize= '18px';
+    listed.style.textAlign='center';
+    listed.style.fontStyle= 'italic'
 
     // delete button
 
 
         let delBtn = document.createElement('button');
-        delBtn.textContent ='Del'
+        delBtn.textContent ='Remove'
         delBtn.className= 'delete'
 
         listedItem.appendChild(delBtn);
 
-        
-        //document.body.appendChild(listed)
+// vanilla js 
+    let clickOnBtn = document.querySelector('.delete')
 
-
-    /* let clickOnBtn = document.querySelector('.delete')
+    
 
     console.log(clickOnBtn);
     clickOnBtn.addEventListener('click',()=>{
 
-        listed.removeChild(listedItem)
+        listed.removeChild(listed.childNodes[0]);
 
-  })*/
+  })
+
+  // jQuery
+
 
       $(document).ready(function(){
         $('.delete').click(function(){
 
             $(listedItem).hide();
-        });
+        }); 
 
 
         // styling the delete button
 
         $('.delete').css({
-            'background-color':'black',
-            'width':'30px',
+            'background-color':'darkred',
+            'width':'auto',
             'color':'white',
             'margin': '15px',
             'text-align': 'center',
+            'cursor':'pointer',
+            'padding': '5px',
 
         });
 });
 });
 
 
-
-
-let element = document.getElementsByClassName('delete');
-element.style.color= 'red';
 
 
 
