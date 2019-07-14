ccc2509e391641f7c4f69d6e831cc7bde802d9e0
yasmin <yasminefawzy22@gmail.com>
2019-07-14 23:02:07 +0200

    http request

--- examples/async.js
+++ examples/async.js
@@ -6,6 +6,26 @@ setTimeout(()=>{
     console.log('the call back function fired');
     
 },2000);
+
+// this is asynchronous function that starts while the files run , its going to wait 2 sec and will finish later by firing this callback function
 console.log(4);
 console.log(5);
 
+
+
+// hTTP request
+
+
+const request = new XMLHttpRequest();
+
+
+request.addEventListener('readystatechange', ()=>{
+    if(request.readyState=== 4){
+        console.log(request.responseText);
+        
+    }
+})
+
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+request.send();
