e6309b3eb970ad6220606b4a11c7c10263f15111
yasmin <yasminefawzy22@gmail.com>
2019-07-15 10:40:37 +0200

    book list

--- exercises/tasks-dom/file.js
+++ exercises/tasks-dom/file.js
@@ -2,27 +2,37 @@ const books = [
     {
       title: 'The Design of EveryDay Things',
       author: 'Don Norman',
-      alreadyRead: false
+      alreadyRead: true,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/41bWcNdTGmL._SX330_BO1,204,203,200_.jpg'
     }, {
       title: 'The Most Human Human',
       author: 'Brian Christian',
-      alreadyRead: true
+      alreadyRead: false,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg'
     }
   ];
 
    books.forEach(book =>{
     
-      return book
-     
+      // return book
+
+      let listed= document.querySelector('#book');
+      let listItem= document.createElement('li')
+      listItem.innerHTML= `<p>${book.title}, ${book.author}</p> <a href='${book.img}'> <img src='${book.img}'> </a>`
+      listed.append(listItem)
+
+     if(book.alreadyRead){
+       listItem.classList.add('alreadyread')
+     }
   })
   
-console.log(books);
+//console.log(books);
 
 // Use a ul and li to display the books.
 
 // first way to add list item 
 
-let output='';
+/* let output='';
 
 for (let i = 0; i < books.length; i++) {
   
@@ -31,16 +41,18 @@ for (let i = 0; i < books.length; i++) {
   output += '<li>' + books[i].alreadyRead+ '</li>'
 }
 
-document.querySelector('#book').innerHTML= output;
+document.querySelector('#book').innerHTML= output; */
 
+/* 
+  styling = document.getElementById('status')
+  for (let i =0; i<styling.length; i++){
+    if(styling[i].alreadyRead === true){
+      styling.style.color = 'red'
+    }
+    else if(styling[i].alreadyRead == true){
+      styling[1].style.color = 'green'
 
-  //styling = document.getElementById('status')
-  // for (let i =0; i<styling.length; i++){
-  //   if(styling[i].alreadyRead === true){
-  //     styling.style.color = 'red'
-  //   }
-    // else if(styling[i].alreadyRead == true){
-    //   styling[1].style.color = 'green'
+ */
     
     
   
