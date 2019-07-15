1ffa17302a2a28765d9ab1e6a1357c403fbffaf4
yasmin <yasminefawzy22@gmail.com>
2019-07-15 15:10:33 +0200

    fetching

--- projects/webpack-prototype/src/index.js
+++ projects/webpack-prototype/src/index.js
@@ -1,6 +1,30 @@
 import {apiKey} from "./token";
 
 
+
+let button= document.querySelector('#btn').addEventListener('click',()=>{
+
+// function weatherBalloon() {
+  var key = apiKey;
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=Berlin&APPID=${key}`)  
+  .then(function(resp) { return resp.json() }) // Convert data to json
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(error) {
+    // catch any errors
+    console.log(error);
+    
+  });
+});
+
+//weatherBalloon()
+
+
+
+
+
+
 /* import createBanner from "./banner.js";
 
 import WebpackImg from "./webpack-logo.png"; */
@@ -26,3 +50,5 @@ document.body.appendChild(
 
 document.body.appendChild(createBanner());
 document.body.appendChild(createImage(WebpackImg)); */
+
+
