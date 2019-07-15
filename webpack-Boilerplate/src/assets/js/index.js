fbcebe3d2b3035db8096b740e7f72fc7df4124d8
yasmin <yasminefawzy22@gmail.com>
2019-07-15 11:25:34 +0200

    js file

---
+++ webpack-Boilerplate/src/assets/js/index.js
@@ -0,0 +1,32 @@
+import "@scss/styles.scss";
+import logoImg from "@img/logo.png";
+import 'bootstrap';
+
+
+window.addEventListener('click', (ev) => {
+    const elm = ev.target;
+    const selector = elm.getAttribute('data-target');
+    collapse(selector, 'toggle');
+}, false);
+
+const fnmap = {
+    'toggle': 'toggle',
+    'show': 'add',
+    'hide': 'remove'
+};
+const collapse = (selector, cmd) => {
+    document.querySelector(selector).classList[fnmap[cmd]]('show');
+}
+
+
+document.querySelector("#logo").src = logo
+
+
+
+
+
+let filename = logoImg.substring(logoImg.lastIndexOf('/') + 1);
+logo.src = `assets/img/${filename}`;
+
+let message = "Hello Webpack";
+console.log(` Message is: ${message}`);
