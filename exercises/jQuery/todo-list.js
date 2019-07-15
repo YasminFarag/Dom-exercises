ac51b83db214c618bbde028897e2600c8d47bdf7
yasmin <yasminefawzy22@gmail.com>
2019-07-15 11:09:21 +0200

    styling

--- exercises/jQuery/todo-list.js
+++ exercises/jQuery/todo-list.js
@@ -24,6 +24,8 @@ divider.addEventListener('click',(e)=>{
    /*  ul.textContent= 'new Tasks'
     myCont.appendChild(ul)
     document.body.appendChild(myCont) */
+
+
 let listed= document.getElementById('list')
     let listedItem= document.createElement('li')
    
@@ -44,14 +46,11 @@ let listed= document.getElementById('list')
     if(e.target.className=== 'delete'){
         e.target.parentElement.remove();
     }
-});
-    //styling listed item
-/* 
-     listed.style.listStyleType= 'none';
-    listed.style.fontSize= '18px';
-    listed.style.textAlign='center';
-    listed.style.fontStyle= 'italic' */
 
+});
+    
+
+     
     // delete button
 
 
@@ -83,19 +82,7 @@ let listed= document.getElementById('list')
         }); 
  */
 
-        // styling the delete button
-
-        $('.delete').css({
-            'background-color':'darkred',
-            'width':'auto',
-            'color':'white',
-            'margin': '15px',
-            'text-align': 'center',
-            'cursor':'pointer',
-            'padding': '5px',
-
-        });
-
+       
 
 
 
