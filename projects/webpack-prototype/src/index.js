f6bdbd6bebdc0676fde6da418ba4829a15b5e7cf
yasmin <yasminefawzy22@gmail.com>
2019-07-16 09:58:55 +0200

    changes

--- projects/webpack-prototype/src/index.js
+++ projects/webpack-prototype/src/index.js
@@ -1,41 +1,12 @@
-import {apiKey} from "./token";
+let button = document.querySelector('#btn')
+button.addEventListener('click', ()=>{
+  let city= document.querySelector('.inpt').value;
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=ac731e61d18f2ff0e0c877aef04eaf71`)
+  .then(function(resp) { return resp.json() })
+.then(function(data) {
+  console.log(data);
+});
 
-let unit= 'celsius'; // present which unit either metric or imperial
-let searchMethod; // represents either zip code or queue 
-
-
-
-
-function searchWeather(term) {
- let key = apiKey;
-  fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${term}&APPID=${key}`)  
-  .then(result=> { return result.json() }) // Convert data to json
-  .then(result => {
-    init(result); // calling the result from the server 
-  })
-  .catch(function(error) {
-    // catch any errors
-    console.log(error);
-    
-  });
-
-}
-searchWeather()
-
-function init(serverResult){
-  
-  switch(serverResult.weather[0].main){
-
-    case 'Clouds':
-    document.body.style.backgroundImage = 'url("clouds-cloudporn-weather-lookup-158163.jpeg")'
-    break;
-  }
-
-}
- 
-
-let button= document.querySelector('.container').addEventListener('click',()=>{
-  let search = document.querySelector('.inpt').value  
 
 });
 
