da24aaddc1602e3688d17586be512b404893a92d
yasmin <yasminefawzy22@gmail.com>
2019-07-17 15:25:47 +0200

    line

--- projects/webpack-prototype/src/index.js
+++ projects/webpack-prototype/src/index.js
@@ -24,70 +24,61 @@ function weatherdemo(serverRequest){
 
     case 'Clear':
     document.body.style.backgroundImage= 'url("./src/sunny.jpeg")';
-    /* let container= document.querySelector('#container') ;
-    container.style.backgroundColor= 'grey';
-    container.style.width= '300px';
-    container.style.height= '200px';
-    container.style.opacity= '0.8';
-    container.style.marginLeft= '520px';
-    container.style.paddingTop= '30px';
-    container.style.color= 'whitesmoke';
-    container.style.borderRadius= '10%'; */
+    let contain= document.querySelector('#container') ;
+    contain.style.backgroundColor= 'mediumblue';
+    contain.style.width= '300px';
+    contain.style.height= '200px';
+    contain.style.opacity= '0.5';
+    contain.style.marginLeft= '520px';
+    contain.style.paddingTop= '30px';
+    contain.style.color= 'black';
+    contain.style.borderRadius= '10%';
+
+
+   
 
     break;
 
     case 'Clouds':
     document.body.style.backgroundImage= 'url("./src/clouds.jpeg")';
-    // let container= document.querySelector('#container') ;
-    // container.style.backgroundColor= 'grey';
-    // container.style.width= '300px';
-    // container.style.height= '200px';
-    // container.style.opacity= '0.8';
-    // container.style.marginLeft= '520px';
-    // container.style.paddingTop= '30px';
-    // container.style.color= 'whitesmoke';
-    // container.style.borderRadius= '10%';
+    let container= document.querySelector('#container') ;
+    container.style.backgroundColor= 'grey';
+    container.style.width= '300px';
+    container.style.height= 'max-content';
+    container.style.opacity= '0.8';
+    container.style.marginLeft= '520px';
+    container.style.paddingTop= '30px';
+    container.style.color= 'whitesmoke';
+    container.style.borderRadius= '10%';
 
     //horizontal line
-    let line=document.querySelector('#line');
+    /* let line=document.querySelector('#line');
     line.style.width= '250px';
     line.style.color='black';
-    line.style.height= '10px';
+    line.style.height= '10px'; */
+
+
+    let line = document.querySelector('#line');
+    let created = document.createElement('HR');
+    
+    line.appendChild(created);
+  
 
     break;
 
     case 'Rain':
     case 'Drizzle':
     case 'Mist':
-    document.body.style.backgroundImage= 'url("./src/cloudy.jpeg")'
-
+    document.body.style.backgroundImage= 'url("./src/cloudy.jpeg")';
+    let con = document.querySelector('#container') ;
     break;
 
     case 'thunderstorm':
     document.body.style.backgroundImage= 'url("./src/lightning.jpg")';
-    // let container= document.querySelector('#container') ;
-    /* container.style.backgroundColor= 'grey';
-    container.style.width= '300px';
-    container.style.height= '200px';
-    container.style.opacity= '0.8';
-    container.style.marginLeft= '520px';
-    container.style.paddingTop= '30px';
-    container.style.color= 'whitesmoke';
-    container.style.borderRadius= '10%'; */
-
     break;
 
     case 'Snow':
     document.body.style.backgroundImage= 'url("./src/snowy.jpg")';
-    //let container= document.querySelector('#container') ;
-    container.style.backgroundColor= 'grey';
-    container.style.width= '300px';
-    container.style.height= '200px';
-    container.style.opacity= '0.8';
-    container.style.marginLeft= '520px';
-    container.style.paddingTop= '30px';
-    container.style.color= 'whitesmoke';
-    container.style.borderRadius= '10%';
 
     break;
     default:
@@ -100,7 +91,7 @@ let weatherDescription= document.querySelector('#weatherdescription')
 let humidity= document.querySelector('#humidty');
 let tempreature= document.querySelector('#tempreature');
 let wind= document.querySelector('#wind');
-let image = document.querySelector('.img')
+ let image = document.querySelector('.img')
 
 // print city name
 cityName.innerHTML=serverRequest.name
@@ -119,15 +110,46 @@ humidity.innerHTML= 'Humidity: ' +  Math.floor(serverRequest.main.humidity) + ' 
 
 wind.innerHTML= 'Wind Speed: ' + Math.floor(serverRequest.wind.speed) + ' km/hr'
 
- image.src= `http://openweathermap.org/img/wn/${serverRequest.weather[0].icon}@2x.png` 
+image.src= `http://openweathermap.org/img/wn/${serverRequest.weather[0].icon}@2x.png` 
 
 
+
+
+/* function loading(){
+  let img = document.querySelector('.img')
+  img.style.visibility= 'hidden'
+}
+
+
+ */
+
+ let hidden= document.querySelector('.hidden');
+ hidden.classList.toggle('hidden')
 
 };
 
 
+function styles(){
 
 
+
+  let container= document.querySelector('#container') ;
+  container.style.backgroundColor= 'grey';
+    container.style.width= '300px';
+    container.style.height= 'max-content';
+    container.style.opacity= '0.8';
+    container.style.marginLeft= '520px';
+    container.style.paddingTop= '30px';
+    container.style.color= 'whitesmoke';
+    container.style.borderRadius= '10%';
+
+
+  let line = document.querySelector('#line');
+  let created = document.createElement('HR');
+    
+    line.appendChild(created);
+  
+}
 
 
 
